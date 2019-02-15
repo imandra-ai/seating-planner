242fc281769a5baec28466b677e0b3ac55f8a085
Dave Aitken <dave.aitken@gmail.com>
2019-02-15 15:08:33 +0000

    encoder names and fix table dups

--- src/App_decoders.ml
+++ src/App_decoders.ml
@@ -18,15 +18,15 @@ module Encode (E : D.Encode.S) = struct
   let intPairs : (int * int) list encoder = fun xs ->
     xs |> list (fun x -> [fst x; snd x] |> list (fun y  -> int y))
 
-  let person_node_of_pair : assignment encoder = fun x ->
+  let person_node : assignment encoder = fun x ->
     obj [("id", string x.guest.name)
         ;("group", int x.table)]
 
-  let table_node_of_pair : assignment encoder = fun x ->
-    obj [("id", string (Printf.sprintf "Table %d" x.table))
-        ;("group", int x.table)]
+  let table_node : int encoder = fun t ->
+    obj [("id", string (Printf.sprintf "Table %d" t))
+        ;("group", int t)]
 
-  let person_link_of_pair : assignment encoder = fun x ->
+  let person_link : assignment encoder = fun x ->
     obj [("source", string (Printf.sprintf "Table %d" x.table))
         ;("target", string x.guest.name)
         ;("value", int 5)
@@ -34,11 +34,11 @@ module Encode (E : D.Encode.S) = struct
 
   type person_or_table =
     | Person of assignment
-    | Table of assignment
+    | Table of int
 
-  let node_of_person_or_table_pair : person_or_table encoder = function
-    | Person p -> person_node_of_pair p
-    | Table t -> table_node_of_pair t
+  let person_or_table_node : person_or_table encoder = function
+    | Person p -> person_node p
+    | Table t -> table_node t
 
   let graph_of_assignments : assignment list encoder = fun xs ->
     let persons =
@@ -47,10 +47,11 @@ module Encode (E : D.Encode.S) = struct
     in
     let tables =
       xs
+      |> CCList.map (fun t -> t.table)
       |> CCList.uniq ~eq:(=)
       |> CCList.map (fun t -> Table t)
     in
-    obj [("nodes", list node_of_person_or_table_pair (persons @ tables))
-        ;("links", (list person_link_of_pair xs))]
+    obj [("nodes", list person_or_table_node (persons @ tables))
+        ;("links", (list person_link xs))]
 
 end [@@program]
